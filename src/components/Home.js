7ac6c2847879c5519507f315c3b03f5fadcdf783
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2023-08-16 16:31:40 +0100

    Add basic info to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
 const Home = () => {
   return (
-    <div>
-      This is home
+    <div className="hero min-h-screen bg-base-200">
+    <div className="hero-content text-center">
+      <div className="max-w-screen-xl space-y-5">
+        <h1 className="text-4xl font-bold">The Global Pneumococcal Sequencing Project</h1>
+        <h3 className="text-2xl">A worldwide genomic survey of the impact of vaccination on the pathogen population</h3>
+      </div>
     </div>
-    
+  </div>
   )
 }
 
