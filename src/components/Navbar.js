4a462b46354fc8c632af545f21374c8b10024c59
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2023-08-16 10:53:42 +0100

    Add all Navbar content

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,81 +1,164 @@
 import { Link } from "react-router-dom";
 
-const NavbarContent = () => {
-  return (
-    <>
-      <li><Link to="/">Home</Link></li>
-      <li>
-        <details>
-          <summary>About</summary>
-          <ul className="p-2">
-            <li><Link to="/outline">Project Outline</Link></li>
-            <li><Link to="/team">The Team</Link></li>
-            <li><Link to="/partners">Project Partners</Link></li>
-            <li><Link to="/substudies">Sub-studies</Link></li>
-          </ul>
-        </details>
-      </li>
-      <li>
-        <details>
-          <summary>Resources</summary>
-          <ul className="p-2">
-            <li><Link to="/overview">Overview</Link></li>
-            <li><Link to="/countries">Countries</Link></li>
-            <li><Link to="/strains">Strains</Link></li>
-            <li><Link to="/gpsc_st_lookup_table">GPSC-ST Lookup Table</Link></li>
-            <li><Link to="/serotypes">Serotypes</Link></li>
-            <li><Link to="/pmen_clones">PMEN Clones</Link></li>
-            <li><Link to="/isolate_bank_request">Isolate Bank Request</Link></li>
-          </ul>
-        </details>
-      </li>
-      <li><Link to="/publications">Publications</Link></li>
-      <li><Link to="/capacity_building">Capacity Building</Link></li>
-      <li><Link to="/gps_database">GPS Database</Link></li>
-      <li><Link to="/gps_visualiser">GPS Visualiser</Link></li>
-      <li>
-        <details>
-          <summary>Training</summary>
-          <ul className="p-2">
-            <li><Link to="/gps_tools">GPS Tools</Link></li>
-            <li><Link to="/bioinformatics_training">Bioinformatics Training</Link></li>
-            <li><Link to="/drag_and_drop_tools">Drag and Drop Tools</Link></li>
-            <li><Link to="/command_line">Command Line</Link></li>
-          </ul>
-        </details>
-      </li>
-      <li><Link to="/contact">Contact</Link></li>
-    </>
-  )
+const NavBarContent = [
+  {
+    title: 'About',
+    submenu: [
+      {
+        title: 'Project Outline',
+        url: '/outline',
+      },
+      {
+        title: 'The Team',
+        url: '/team',
+      },
+      {
+        title: 'Projet Partners',
+        url: '/partners',
+      },
+      {
+        title: 'Sub-studies',
+        url: '/substudies',
+      },
+    ]
+  },
+  {
+    title: 'Resources',
+    submenu: [
+      {
+        title: 'Overview',
+        url: '/overview',
+      },
+      {
+        title: 'Countries',
+        url: '/countries',
+      },
+      {
+        title: 'Strains',
+        url: '/strains',
+      },
+      {
+        title: 'GPSC-ST Lookup Table',
+        url: '/gpsc_st_lookup_table',
+      },
+      {
+        title: 'Serotypes',
+        url: '/serotypes',
+      },
+      {
+        title: 'PMEN Clones',
+        url: '/pmen_clones',
+      },
+      {
+        title: 'Isolate Bank Request',
+        url: '/isolate_bank_request',
+      },
+    ]
+  },
+  {
+    title: 'Publications',
+    url: '/publications'
+  },
+  {
+    title: 'Capacity Building',
+    url: '/capacity_building'
+  },
+  {
+    title: 'GPS Database',
+    url: '/gps_database'
+  },
+  {
+    title: 'GPS Visualiser',
+    url: '/gps_visualiser'
+  },
+  {
+    title: 'Training',
+    submenu: [
+      {
+        title: 'GPS Tools',
+        url: '/gps_tools',
+      },
+      {
+        title: 'Bioinformatics Training',
+        url: '/bioinformatics_training',
+      },
+      {
+        title: 'Drag and Drop Tools',
+        url: '/drag_and_drop_tools',
+      },
+      {
+        title: 'Command Line',
+        url: '/command_line',
+      },
+    ]
+  },
+  {
+    title: 'Contact',
+    url: '/contact'
+  }
+]
+
+const deactivate = () => {
+  const elem = document.activeElement
+  if (elem) { elem.blur() }
 }
 
-const Navbar = () => {
-  return (
-    <div className="navbar bg-primary text-primary-content">
-      <div className="flex-1">
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-          </label>
-          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-            <NavbarContent />
-          </ul>
-        </div>
-        <Link to="/" className="btn btn-ghost normal-case">
-          <img className='w-10' src='gps_logo.png' alt="GPS Project Logo"></img>
-          <div>
-            <div className="text-xl text-left">GPS</div>
-            <div className="text-xs text-left">Global Pneumococcal Sequencing Project</div>
-          </div>
-        </Link>
-      </div>
-      <div className="flex-none hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <NavbarContent />
+const Button = ({ title, url }) => (
+  <Link onClick={deactivate} to={url}><label tabIndex={0} className="btn btn-ghost btn-xs rounded-btn">{title}</label></Link>
+)
+
+const DropdownContent = ({ submenu }) => (
+  submenu.map(( item, index ) => (
+    <li onClick={deactivate} key={index}><Link to={item.url}>{item.title}</Link></li>
+  ))
+)
+
+const DropdownButton = ({ title, submenu }) => (
+  <>
+    <div className="dropdown dropdown-hover dropdown-end">
+    <label tabIndex={0} className="btn btn-ghost btn-xs rounded-btn">{title}</label>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        <DropdownContent submenu={submenu}/>
+      </ul>
+    </div>
+  </>
+)
+
+const NavbarItems = () => (
+  NavBarContent.map((content, index) => (
+    content.submenu ?
+      <DropdownButton title={content.title} submenu={content.submenu} key={index}/>
+    :
+      <Button title={content.title} url={content.url} key={index} />
+  ))
+)
+
+const Navbar = () => (
+  <div className="navbar bg-primary text-primary-content h-20">
+    <div className="flex-1">
+      <div className="dropdown">
+        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        </label>
+        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+          <NavbarItems />
         </ul>
       </div>
+      <Link to="/" className="btn btn-ghost normal-case h-20">
+        <img className='w-10' src='gps_logo.png' alt="GPS Project Logo"></img>
+        <div>
+          <div className="text-xl text-left">GPS</div>
+          <div className="text-xs text-left">Global Pneumococcal</div>
+          <div className="text-xs text-left">Sequencing Project</div>
+        </div>
+      </Link>
     </div>
-  );
-}
+    <div className="flex-none hidden lg:flex">
+      <ul className="menu menu-horizontal px-1">
+        <NavbarItems />
+      </ul>
+    </div>
+  </div>
+)
 
 export default Navbar
