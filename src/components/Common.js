71365bd1536bc4512ecb1fae034c93dece3eeff8
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2023-08-24 02:52:53 +0100

    Move common components to Common.js

--- src/components/Common.js
+++ src/components/Common.js
@@ -1,4 +1,6 @@
 import ReactMarkdown from 'react-markdown'
+import { Link } from "react-router-dom"
+
 
 const TitleText = ({text}) => (
   <h1 className="text-4xl font-bold"><ReactMarkdown children={text} /></h1>
@@ -9,13 +11,14 @@ const SubtitleText = ({text}) => (
 )
 
 const ContentText = ({text}) => (
-  <div className="text-lg">
+  <div className="w-full text-lg">
     <ReactMarkdown 
       components={{
-        a: ({ href, children })  => <a href={href} className='link'>{children}</a>,
+        a: ({ href, children })  => <Link to={href} className='link' target='_blank'>{children}</Link>,
         ol: ({ start, children }) => <ol start={start} className='list-decimal list-outside ms-8'>{children}</ol>,
         ul: ({ children }) => <ul className='list-disc list-outside ms-8'>{children}</ul>,
-        h3: ({children}) => <h3 className='text-xl font-bold'>{children}</h3>
+        h3: ({children}) => <h3 className='text-xl font-bold'>{children}</h3>,
+        img: ({src, alt, title}) => <img className='w-full' src={src} alt={alt} title={title}/>
       }} 
       children={text} 
     />
@@ -30,4 +33,22 @@ const ContentCode = ({code}) => (
   </div>
 )
 
-export {TitleText, SubtitleText, ContentText, ContentCode}
+const SectionContent = ({text, code}) => (
+  text
+  ? <ContentText text={text} />
+  : code 
+    ? <ContentCode code={code} />
+    : ''
+)
+
+const Section = ({subtitle, content}) => (
+  <div className="hero-content flex-col w-full items-start">
+    <SubtitleText text={subtitle} />
+    {
+      content.map( (props, index) => 
+        <SectionContent {...props} key={index} />)
+    }
+  </div>
+)
+
+export {TitleText, SubtitleText, ContentText, ContentCode, Section}
