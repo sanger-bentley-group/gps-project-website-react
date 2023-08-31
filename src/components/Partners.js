5057ecf0ec58a43bab9d7c7e8731f634f52301ff
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2023-08-31 17:27:12 +0100

    Add tooltip to display partner info

--- src/components/Partners.js
+++ src/components/Partners.js
@@ -1,37 +1,67 @@
-import { ComposableMap, Geographies, Geography, Sphere, Graticule } from "react-simple-maps"
+import { renderToStaticMarkup } from 'react-dom/server'
+import { ComposableMap, Geographies, Geography, Sphere, Graticule, Marker } from "react-simple-maps"
 import { Tooltip } from "react-tooltip"
+
 import { TitleText } from "./Common"
 
 import content from '../content/partnersContent'
 import topoJSON from '../content/worldTopoJSON.json'
 
-const Partners = () => {
+const TooltipContent = ({ info }) => (
+  <div className='text-center space-y-4'>
+    <div className='italic'>{info.country}, {info.city}</div>
+    {
+      info.affiliations.map((affiliation, index) => (
+        <div key={index}>
+          <div className='font-bold'>{affiliation.name}</div>
+          <div>{affiliation.partners.join(', ')}</div>
+        </div>
+      ))
+    }
+  </div>
+)
 
-  const Map = () => (
-    <ComposableMap width={1000} height={500} >
-      <Sphere stroke="#AAAAAA" strokeWidth={0.5} />
-      <Graticule stroke="#AAAAAA" strokeWidth={0.5} />
-      <Geographies geography={topoJSON}>
-        {({ geographies }) =>
-          geographies.map((geo) => (
-            <Geography key={geo.rsmKey} geography={geo} />
-          ))
-        }
-      </Geographies>
-    </ComposableMap>
-  )
+const Markers = () => (
+  content.partnerInfo.map((info, index) => (
+    <Marker coordinates={[info.longitude, info.latitude]} key={index} >
+      <circle 
+        r={5} 
+        fill="hsl(var(--p))" 
+        stroke="#FFFFFF"
+        strokeWidth={1}
+        data-tooltip-id="partner-info"
+        data-tooltip-html={renderToStaticMarkup(<TooltipContent info={info} />)}
+        data-tooltip-place="top"
+      />
+    </Marker>
+  ))
+)
 
-  return (
-    <div className="hero h-full bg-base-200 flex flex-col justify-center space-y-10 py-20">
-      <div className="hero-content w-full flex-col text-center">
-        <TitleText text={content.title}/>
-      </div>
-      <div className="hero-content w-full">
-        <Map />
-        <Tooltip id="partner-info" />
-      </div>
+const Map = () => (
+  <ComposableMap width={1000} height={500} >
+    <Sphere stroke="#AAAAAA" strokeWidth={0.5} />
+    <Graticule stroke="#AAAAAA" strokeWidth={0.5} />
+    <Geographies geography={topoJSON}>
+      {({ geographies }) =>
+        geographies.map((geo) => (
+          <Geography key={geo.rsmKey} geography={geo} />
+        ))
+      }
+    </Geographies>
+    <Markers />
+  </ComposableMap>
+)
+
+const Partners = () => (
+  <div className="hero h-full bg-base-200 flex flex-col justify-center space-y-10 py-20">
+    <div className="hero-content w-full flex-col text-center">
+      <TitleText text={content.title}/>
     </div>
-  )
-}
+    <div className="hero-content w-full">
+      <Map />
+      <Tooltip id="partner-info" />
+    </div>
+  </div>
+)
 
 export default Partners
