a7dba231a0396416b30e3a94b73e53a81b62ba33
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2023-09-06 17:17:09 +0100

    Add Bioinformatics Training section

--- src/components/Training.js
+++ src/components/Training.js
@@ -1,9 +1,11 @@
+import BioinformaticsTraining from "./BioinformaticsTraining"
 import CommandLine from "./CommandLine"
 import GPSTools from "./GPSTools"
 
 const Training = () => (
   <div className="h-full">
     <section id="gps-tools"><GPSTools /></section>
+    <section id="bioinformatics-training"><BioinformaticsTraining /></section>
     <section id="command-line"><CommandLine /></section>
   </div>
 )
---
+++ src/content/bioinformaticsTrainingContent.js
@@ -0,0 +1,22 @@
+const content = {
+  title: "Bioinformatics Training",
+  sections: [
+    {
+      content: [
+        {
+          type: 'md',
+          content: 'To achieve an ongoing genomic surveillance in LMICs, we encourage GPS partners to generate and analyse data locally. For the latter, GPS2, together with our sister project [JUNO](https://www.gbsgen.net/), develop a series of online bioinformatics training modules and they are freely available at our training site.'
+        },
+        {
+          type: 'button',
+          content: {
+            text: 'Access the Training Site',
+            url: 'https://training.bactgen.sanger.ac.uk/',
+          }
+        },
+      ]
+    },
+  ]
+}
+
+export default content
