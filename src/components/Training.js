1c19715b61461fe3b7e5ff89a76489c044b331a1
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2023-09-06 18:20:47 +0100

    Add Drag and Drop Tools section

--- src/components/Training.js
+++ src/components/Training.js
@@ -1,11 +1,13 @@
-import BioinformaticsTraining from "./BioinformaticsTraining"
-import CommandLine from "./CommandLine"
 import GPSTools from "./GPSTools"
+import BioinformaticsTraining from "./BioinformaticsTraining"
+import DragAndDropTools from "./DragAndDropTools"
+import CommandLine from "./CommandLine"
 
 const Training = () => (
   <div className="h-full">
     <section id="gps-tools"><GPSTools /></section>
     <section id="bioinformatics-training"><BioinformaticsTraining /></section>
+    <section id="drag-and-drop-tools"><DragAndDropTools /></section>
     <section id="command-line"><CommandLine /></section>
   </div>
 )
---
+++ src/content/dragAndDropToolsContent.js
@@ -0,0 +1,116 @@
+const content = {
+  title: "Drag and Drop Tools",
+  sections: [
+    {
+      content: [
+        {
+          type: 'md',
+          content: 'Below are tutorial videos to illustrate how to use web-based tools to perform bioinformatic analysis and to visualise analysed data.',
+        },
+      ]
+    },
+    {
+      content: [
+        {
+          type: 'logo',
+          content: {
+            logo: 'pathogenwatch_logo.svg',
+            url: 'https://pathogen.watch/genomes/all?genusId=1301&speciesId=1313',
+            alt: 'Click to access Pathogenwatch website'
+          }
+        },
+        {
+          type: 'md',
+          content: '*In silico serotype, GPSC, MLST and antibiotic resistance*'
+        },
+        {
+          type: 'youtube',
+          content: {
+            title: 'Pathogenwatch for Streptococcus pneumoniae',
+            id: 'Q8bDuZZ3hXg'
+          }
+        },
+        {
+          type: 'button',
+          content: {
+            text: 'Download Example Assemblies',
+            url: 'example-assemblies.zip',
+          }
+        },
+      ]
+    },
+    {
+      content: [
+        {
+          type: 'logo',
+          content: {
+            logo: 'microreact_logo.svg',
+            url: 'https://microreact.org/',
+            alt: 'Click to access Microreact website'
+          }
+        },
+        {
+          type: 'md',
+          content: '*Visualisation of phylogeny with temporal and spatial metadata*'
+        },
+        {
+          type: 'md',
+          content: '[Microreact Introduction](https://vimeo.com/393391441) from [Genomic Pathogen Surveillance](https://vimeo.com/cgps) on Vimeo'
+        },
+        {
+          type: 'vimeo',
+          content: {
+            title: 'Pathogenwatch for Streptococcus pneumoniae',
+            id: '393391441'
+          }
+        },
+        {
+          type: 'button',
+          content: {
+            text: 'Download Example Data',
+            url: 'microreact-examples.zip',
+          }
+        },
+      ]
+    },
+    {
+      content: [
+        {
+          type: 'logo',
+          content: {
+            logo: 'phandango_logo.png',
+            url: 'https://jameshadfield.github.io/phandango/#/gps',
+            alt: 'Click to access phandango website'
+          }
+        },
+        {
+          type: 'md',
+          content: '*Visualisation of recombination and pangenome analysis*'
+        },
+        {
+          type: 'youtube',
+          content: {
+            title: 'Phandango',
+            id: 'pGgtt2z8sUI'
+          }
+        },
+        {
+          type: 'button',
+          content: {
+            text: 'Download Example Recombination Data',
+            url: 'recombination-examples.zip',
+          }
+        },
+        {
+          type: 'button',
+          content: {
+            text: 'Download Pan Genome Data',
+            url: 'pangenome-examples.zip',
+          }
+        },
+      ]
+    },
+  ]
+}
+
+export default content
