64971d58f6929a0cc9b9b54c31372c9d179da5cd
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2023-09-18 22:25:38 +0100

    Add publications page

---
+++ src/content/publicationsContent.js
@@ -0,0 +1,104 @@
+const content = {
+  title: "Publications",
+  sections: [
+    {
+      subtitleLogo: {
+        content: '[Global Pneumococcal Sequencing Collection](https://www.microbiologyresearch.org/content/global-pneumococcal-sequencing-collection)',
+        logo: {
+          url: 'images/microbiology-society-logo.png',
+          alt: 'Microbiology Society Logo'
+        }
+      },
+      content: [
+        {
+          type: 'publicationGrid',
+          content: [
+            {
+              title: 'Key features of pneumococcal isolates recovered in Central and Northwestern Russia in 2011-2018 determined through whole-genome sequencing',
+              platform: 'Microbial Genomics',
+              buttons: [
+                {
+                  text: 'Journal',
+                  url: 'https://www.microbiologyresearch.org/content/journal/mgen/10.1099/mgen.0.000851'
+                }
+
+              ]
+            },
+            {
+              title: 'Distinct *Streptococcus pneumoniae* cause invasive disease in Papua New Guinea',
+              platform: 'Microbial Genomics',
+              buttons: [
+                {
+                  text: 'Journal',
+                  url: 'https://www.microbiologyresearch.org/content/journal/mgen/10.1099/mgen.0.000835'
+                },
+              ]
+            },
+            {
+              title: 'Genetic background of Cambodian pneumococcal carriage isolates following pneumococcal conjugate vaccine 13',
+              platform: 'Microbial Genomics',
+              buttons: [
+                {
+                  text: 'Journal',
+                  url: 'https://www.microbiologyresearch.org/content/journal/mgen/10.1099/mgen.0.000837'
+                },
+              ]
+            },
+            {
+              title: 'Population genomics of pneumococcal carriage in South Africa following the introduction of the 13-valent pneumococcal conjugate vaccine (PCV13) immunization',
+              platform: 'Microbial Genomics',
+              buttons: [
+                {
+                  text: 'Journal',
+                  url: 'https://www.microbiologyresearch.org/content/journal/mgen/10.1099/mgen.0.000831'
+                },
+              ]
+            },
+            {
+              title: 'A *Streptococcus pneumoniae* lineage usually associated with pneumococcal conjugate vaccine (PCV) serotypes is the most common cause of serotype 35B invasive disease in South Africa, following routine use of PCV',
+              platform: 'Microbial Genomics',
+              buttons: [
+                {
+                  text: 'Journal',
+                  url: 'https://www.microbiologyresearch.org/content/journal/mgen/10.1099/mgen.0.000746'
+                },
+              ]
+            },
+            {
+              title: 'Genomic surveillance of invasive *Streptococcus pneumoniae* isolates in the period pre-PCV10 and post-PCV10 introduction in Brazil',
+              platform: 'Microbial Genomics',
+              buttons: [
+                {
+                  text: 'Journal',
+                  url: 'https://www.microbiologyresearch.org/content/journal/mgen/10.1099/mgen.0.000635'
+                },
+              ]
+            },
+            {
+              title: 'Population genetic structure, serotype distribution and antibiotic resistance of *Streptococcus pneumoniae* causing invasive disease in children in Argentina',
+              platform: 'Microbial Genomics',
+              buttons: [
+                {
+                  text: 'Journal',
+                  url: 'https://www.microbiologyresearch.org/content/journal/mgen/10.1099/mgen.0.000636'
+                },
+              ]
+            },
+            {
+              title: '*Streptococcus pneumoniae* genomic datasets from an Indian population describing pre-vaccine evolutionary epidemiology using a whole genome sequencing approach',
+              platform: 'Microbial Genomics',
+              buttons: [
+                {
+                  text: 'Journal',
+                  url: 'https://www.microbiologyresearch.org/content/journal/mgen/10.1099/mgen.0.000645'
+                },
+              ]
+            },
+          ]
+        },
+      ]
+    },
+  ]
+}
+
+export default content
