b881c876622f6dc60e94d76628e4ae0a96779358
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2023-09-25 22:49:17 +0100

    Remove text content from Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,12 +23,12 @@ const Footer = () => (
   <footer className="footer p-10 bg-base-300 justify-between flex flex-col items-center xl:items-start xl:flex-row">
     <img className='w-36 min-w-[9rem]' src='images/sanger_logo.png' alt="Wellcome Sanger Institute Logo"></img>
     <LinksWithDivider
-      title={'Contacts'}
-      items={content.contacts}
+      title={content.contacts.title}
+      items={content.contacts.content}
     />
     <LinksWithDivider
-      title={'Sites'}
-      items={content.sites}
+      title={content.sites.title}
+      items={content.sites.content}
     />
   </footer>
 )
--- src/content/footerContent.js
+++ src/content/footerContent.js
@@ -1,36 +1,42 @@
 const content = {
-  sites: [
-    {
-      title: "Bentley Group",
-      url: "https://bentleygroup.sanger.ac.uk/"
-    },
-    {
-      title: "JUNO",
-      url: "https://www.gbsgen.net/"
-    },
-    {
-      title: "Training",
-      url: "https://training.bactgen.sanger.ac.uk"
-    },
-  ],
-  contacts: [
-    {
-      title: "GPS Project",
-      url: "mailto:gps@pneumogen.net"
-    },
-    {
-      title: "Stephen Bentley",
-      url: "mailto:sdb@sanger.ac.uk"
-    },
-    {
-      title: "Lesley McGee",
-      url: "mailto:lmcgee@cdc.gov"
-    },
-    {
-      title: "Stephanie Lo",
-      url: "mailto:stephanie.lo@sanger.ac.uk"
-    },
-  ]
+  sites: {
+    title: 'Sites',
+    content:  [
+      {
+        title: "Bentley Group",
+        url: "https://bentleygroup.sanger.ac.uk/"
+      },
+      {
+        title: "JUNO",
+        url: "https://www.gbsgen.net/"
+      },
+      {
+        title: "Training",
+        url: "https://training.bactgen.sanger.ac.uk"
+      },
+    ]
+  },
+  contacts: {
+    title: 'Contacts',
+    content: [
+      {
+        title: "GPS Project",
+        url: "mailto:gps@pneumogen.net"
+      },
+      {
+        title: "Stephen Bentley",
+        url: "mailto:sdb@sanger.ac.uk"
+      },
+      {
+        title: "Lesley McGee",
+        url: "mailto:lmcgee@cdc.gov"
+      },
+      {
+        title: "Stephanie Lo",
+        url: "mailto:stephanie.lo@sanger.ac.uk"
+      },
+    ]
+  }
 }
 
 export default content
