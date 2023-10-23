54f313c6afb50e9461891167a0bca59572e53abf
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2023-10-23 16:56:11 +0100

    Move background colour class to base component

--- src/components/Team.js
+++ src/components/Team.js
@@ -3,7 +3,7 @@ import { TitleText, Section } from "./Common"
 import content from '../content/teamContent'
 
 const Team = () => (
-  <div className="hero bg-base-200 flex flex-col justify-center space-y-10 py-20">
+  <div className="hero flex flex-col justify-center space-y-10 py-20">
     <div className="hero-content w-full flex-col text-center">
       <TitleText text={content.title}/>
     </div>
