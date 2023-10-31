319db73f6391a06d55536d52f38ee6b9a11df261
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2023-10-31 18:09:49 +0000

    Remove obsolete file

--- src/utilities/filters.js
+++
@@ -1,9 +0,0 @@
-const TextSearchFilter = ({ column: { filterValue, setFilter }}) => (
-  <input className="input input-xs"
-    value={filterValue || ""}
-    onChange={(e) => { setFilter(e.target.value || undefined) }}
-    placeholder={`Search...`}
-  />
-)
-
-export { TextSearchFilter }
