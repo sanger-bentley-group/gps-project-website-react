198a6d538b8abb587ab872e89ec3380a72242293
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2024-08-27 15:19:06 +0100

    Move comment to tooltip, add cps cluster column

--- src/components/Serotypes.js
+++ src/components/Serotypes.js
@@ -18,7 +18,25 @@ const Serortype = () => {
     {
       header: "Serotype",
       accessorKey: "serotype",
-      enableColumnFilter: true
+      enableColumnFilter: true,
+      cell: props => {
+        const cellValue = props.getValue()
+        const commentvalue = props.row.original.comments
+
+        if (commentvalue === '-') {
+          return cellValue
+        } else {
+          return (
+            <div 
+              className="tooltip" 
+              data-tooltip-id="react-tooltip"
+              data-tooltip-html={commentvalue}
+            >
+              <span className="link whitespace-nowrap">{cellValue}</span>
+            </div>
+          )
+        }
+      }
     },
     {
       header: "Accession Number",
@@ -42,10 +60,6 @@ const Serortype = () => {
       }
     },
     {
-      header: "Comments",
-      accessorKey: "comments",
-    },
-    {
       header: "Reference",
       accessorKey: "reference",
       cell: props => {
@@ -64,6 +78,30 @@ const Serortype = () => {
         }
       }
     },
+    {
+      header: <span><i>cps</i> Gene Cluster</span>,
+      accessorKey: "serotype",
+      id: "serotype_cps",
+      cell: props => {
+        const cellValue = props.cell.getValue().replaceAll(' ', '_')
+        const imgUrl = `img/serotype_cps/serotype_${cellValue}_cps.svg`
+
+        return (
+          <img
+            className="min-w-96"
+            src={imgUrl}
+            alt={`Chart of Serotype ${cellValue} cps region`}
+            key={cellValue}
+            onError={(event) => {
+              event.target.src = 'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="0" height="0"></svg>'
+              event.target.className = 'h-0 w-0'
+              event.target.alt= ''
+            }} 
+          />
+        );
+      }
+    },
+    
   ], [])
 
   return (
