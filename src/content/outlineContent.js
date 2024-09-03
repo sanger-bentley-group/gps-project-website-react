ee6f54cab6566dfaaa4045a627453aad8c986691
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2024-09-03 16:20:48 +0100

    Typo fix

--- src/content/outlineContent.js
+++ src/content/outlineContent.js
@@ -31,7 +31,7 @@ const content = {
         },
         {
           type: 'md',
-          content: '###### The table above shows the serotypes covered by different pneumococcal vaccines. A blue block in the table indicates the serotype of its column is covered by that vaccine of its row. The shade of the colour represents how frequently that serotype is being covered across all vaccines. The darker the color, the more frequently the serotype is being covered.'
+          content: '###### The table above shows the serotypes covered by different pneumococcal vaccines. A blue block in the table indicates the serotype of its column is covered by that vaccine of its row. The shade of the colour represents how frequently that serotype is being covered across all vaccines. The darker the colour, the more frequently the serotype is being covered.'
         },
       ]
     },
