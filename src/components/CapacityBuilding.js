b6a2a694f84726ed62670e2c51a72e02c056dbe2
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2024-09-03 15:21:59 +0100

    Restructure the website

--- src/components/CapacityBuilding.js
+++ src/components/CapacityBuilding.js
@@ -1,15 +1,24 @@
+import LocalSequencing from "./LocalSequencing"
+import LocalDataAnalysis from "./LocalDataAnalysis"
+import OneToOneTraining from "./OneToOneTraining"
+
 import { TitleText, Section } from "./Common"
 
 import content from '../content/capacityBuildingContent'
 
 const CapacityBuilding = () => (
-  <div className="hero flex flex-col justify-center space-y-10 py-20 h-full">
-    <div className="hero-content w-full flex-col text-center">
-      <TitleText text={content.title}/>
+  <div className="h-full">
+    <div className="hero flex flex-col justify-center space-y-10 py-20">
+      <div className="hero-content w-full flex-col text-center">
+        <TitleText text={content.title}/>
+      </div>
+      {content.sections.map( (props, index) => 
+        <Section {...props} key={index} />
+      )}
     </div>
-    {content.sections.map( (props, index) => 
-      <Section {...props} key={index} />
-    )}
+    <section id="local-sequencing"><LocalSequencing /></section>
+    <section id="local-data-analysis"><LocalDataAnalysis /></section>
+    <section id="one-to-one-training"><OneToOneTraining /></section>
   </div>
 )
 
---
+++ src/content/gpsPipelineContent.js
@@ -0,0 +1,22 @@
+const content = {
+  title: "GPS Pipeline",
+  sections: [
+    {
+      subtitle: {
+        content: 'A Portable Pipeline to Reduce the Bioinformatics Burden'
+      },
+      content: [
+        {
+          type: 'md',
+          content: "Ensuring quality control of sequence data and generating succinct, reliable data characterising genomes can be time consuming and require substantial bioinformatic training. During GPS2, a portable pipeline named GPS Pipeline is being developed which will enable collaborators to easily input whole genome sequencing data and generate the *in silico* typing data required for downstream analysis. Although the pipeline is still under active development, its latest version is available on GitHub for testing: [https://github.com/sanger-bentley-group/gps-pipeline/](https://github.com/sanger-bentley-group/gps-pipeline/)"
+        },
+        {
+          type: 'md',
+          content: '![GPS Pipeline Flowchart](img/gps_pipeline_flowchart.drawio.png)'
+        },
+      ]
+    },
+  ]
+}
+
+export default content
