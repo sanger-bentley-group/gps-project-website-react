203180ec3da0300da01a6a107edd289955a899d6
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2024-09-18 11:52:03 +0100

    Add Vaccine History

---
+++ src/content/vaccineHistoryContent.js
@@ -0,0 +1,35 @@
+const content = {
+  title: 'Vaccine History',
+  sections: [
+    {
+      content: [
+        {
+          type: 'timeline',
+          content: [
+            {
+              time: '1881',
+              content: "First discovered independently isolated by Louis Pasteur (deemed 'microbe septicemique de salive') in France from the saliva of a rabies patient as well as by George Pasteur in the U.S., who named it 'Micrococcus pasteuri'",
+              reference: "https://maidenlab.zoo.ox.ac.uk/history-and-biology-istreptococcus-pneumoniaei"
+            },
+            {
+              time: '1886',
+              content: "Renamed 'pneumococcus' for its close association with pneumonia",
+              reference: "https://maidenlab.zoo.ox.ac.uk/history-and-biology-istreptococcus-pneumoniaei"
+            },
+            {
+              time: '1911',
+              content: "Start of efforts to develop an effective pneumococcal vaccine",
+              reference: "https://www.cdc.gov/pinkbook/hcp/table-of-contents/index.html"
+            },
+          ]
+        },
+        {
+          type: 'md',
+          content: '###### <TODO: Credits>'
+        },
+      ]
+    }
+  ]
+}
+
+export default content
