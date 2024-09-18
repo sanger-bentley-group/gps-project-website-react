50dc751f114eef0451af6b56f9681a3dcb58faa5
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2024-09-18 11:52:03 +0100

    Add Vaccine History

--- src/components/SeroBAnk.js
+++ src/components/SeroBAnk.js
@@ -1,7 +1,10 @@
 import Serotypes from "./Serotypes"
+import VaccineHistory from "./VaccineHistory"
+
 const SeroBAnk = () => (
   <div className="h-full">
     <section id="serotypes"><Serotypes /></section>
+    <section id="vaccine-history"><VaccineHistory /></section>
   </div>
 )
 
--- src/content/navbarContent.js
+++ src/content/navbarContent.js
@@ -55,6 +55,10 @@ const content = [
       {
         title: 'Serotypes',
         url: '/serobank#serotypes',
+      },
+      {
+        title: 'Vaccine History',
+        url: '/serobank#vaccine-history',
       }
     ]
   },
