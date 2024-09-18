d60cfb64581bc6508d4d01e41191f756fdbe5bce
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2024-09-18 12:27:32 +0100

    Update vaccine history content

--- src/content/vaccineHistoryContent.js
+++ src/content/vaccineHistoryContent.js
@@ -8,7 +8,7 @@ const content = {
           content: [
             {
               time: '1881',
-              content: "First discovered independently isolated by Louis Pasteur (deemed 'microbe septicemique de salive') in France from the saliva of a rabies patient as well as by George Pasteur in the U.S., who named it 'Micrococcus pasteuri'",
+              content: "First independently discovered by Louis Pasteur (who called it *microbe septicemique de la salive*) in France from the saliva of a rabies patient, and by George M. Sternberg in the U.S. (who named it *Micrococcus pasteuri*)",
               reference: "https://maidenlab.zoo.ox.ac.uk/history-and-biology-istreptococcus-pneumoniaei"
             },
             {
@@ -21,11 +21,66 @@ const content = {
               content: "Start of efforts to develop an effective pneumococcal vaccine",
               reference: "https://www.cdc.gov/pinkbook/hcp/table-of-contents/index.html"
             },
+            {
+              time: '1920',
+              content: "Renamed as *Diplococcus pneumoniae* since it was typically observed in pairs",
+              reference: "https://maidenlab.zoo.ox.ac.uk/history-and-biology-istreptococcus-pneumoniaei"
+            },
+            {
+              time: '1928',
+              content: "The transformation of life was demonstrated by Frederick Griffith, where harmless pneumococci were turned into a lethal form through the co-inoculation of live harmless pneumococci with heat-killed virulent pneumococci in a mouse; This experiment played a vital role in identifying DNA as the genetic material",
+              reference: "https://maidenlab.zoo.ox.ac.uk/history-and-biology-istreptococcus-pneumoniaei"
+            },
+            {
+              time: '1929',
+              content: "The first 32 serotypes were described by Georgia Cooper",
+              reference: "https://doi.org/10.1084/jem.49.3.461"
+            },
+            {
+              time: '1939',
+              content: "Danish Prince Valdemar died of pneumonia and his failed treatment with serogroup 9 antisera revealed additional diversity in serogroup 9, allowing for the identification of serotype 9V",
+              reference: "https://www.cdc.gov/pinkbook/hcp/table-of-contents/index.html"
+            },
+            {
+              time: '1960s',
+              content: "Start of efforts to develop a polyvalent pneumococcal vaccine",
+              reference: "https://www.cdc.gov/pinkbook/hcp/table-of-contents/index.html"
+            },
+            {
+              time: '1974',
+              content: "Renamed *Streptococcus pneumoniae* for its similarities in chain formation similar to other *Streptococcus spp.*",
+              reference: "https://maidenlab.zoo.ox.ac.uk/history-and-biology-istreptococcus-pneumoniaei"
+            },
+            {
+              time: '1977',
+              content: "First pneumococcal polysaccharide vaccine licensed in U.S., contained which antigens from 14 serotypes of pneumococci",
+              reference: "https://www.cdc.gov/pinkbook/hcp/table-of-contents/index.html"
+            },
+            {
+              time: '1983',
+              content: "The 23-valent polysaccharide vaccine was licensed (PPSV23), replacing the 14-valent",
+              reference: "https://www.cdc.gov/pinkbook/hcp/table-of-contents/index.html"
+            },
+            {
+              time: '2000',
+              content: "First conjugate pneumococcal vaccine licensed in U.S., covering 7 serotypes (PCV7)",
+              reference: "https://www.cdc.gov/pinkbook/hcp/table-of-contents/index.html"
+            },
+            {
+              time: '2010',
+              content: "Introduction of 13-valent pneumococcal conjugate vaccine (PCV13), covering 6 more serotypes in addition to PCV7",
+              reference: "https://www.cdc.gov/pinkbook/hcp/table-of-contents/index.html"
+            },
+            {
+              time: '2020',
+              content: "The 100th serotype, 10D, is identified",
+              reference: "https://doi.org/10.1128/mBio.00937-20"
+            },
           ]
         },
         {
           type: 'md',
-          content: '###### <TODO: Credits>'
+          content: '###### The above timeline is based on the [curation](https://pneumococcalcapsules.github.io/serotypes/) of Chessley B. Blacklock, Daniel M. Weinberger, Stephanie Perniciaro, and Anne L. Wyllie at the Yale School of Public Health'
         },
       ]
     }
