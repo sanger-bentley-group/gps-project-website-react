113c7f80148475cfacb4d3863d725bb39aaab251
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2025-05-01 09:56:34 +0100

    Enable TailwindCSS in Vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'
 import dsv from '@rollup/plugin-dsv';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-    plugins: [react(), dsv(), svgr()],
+    plugins: [react(), tailwindcss(), dsv(), svgr()],
     base: '/gps/',
     build: {
         outDir: 'gps',
