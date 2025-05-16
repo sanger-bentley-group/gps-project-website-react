21691e1ce38a00639fcd3753b0627baaea54f4be
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2025-05-16 15:31:00 +0100

    Add vite-imagetools plugin

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
 import dsv from '@rollup/plugin-dsv';
 import svgr from 'vite-plugin-svgr';
+import { imagetools } from 'vite-imagetools';
 
 export default defineConfig({
-    plugins: [react(), tailwindcss(), dsv(), svgr()],
+    plugins: [react(), tailwindcss(), dsv(), svgr(), imagetools()],
     base: '/gps/',
     build: {
         outDir: 'gps',
