0f1280f6e2bb9b8b05bb31be8bfda8694e25f57f
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2026-02-11 11:58:47 +0000

    Update SeroBAnk reference

--- src/content/serotypeContent.js
+++ src/content/serotypeContent.js
@@ -5,7 +5,7 @@ const content = {
       content: [
         {
           type: 'md',
-          content: 'Use the below table to see all the known serotypes and their associated capsular genes. If you find this resource useful for your research, please consider citing [this preprint](https://doi.org/10.1101/2025.04.16.648953 ) in your work.'        
+          content: 'Use the below table to see all the known serotypes and their associated capsular genes. If you find this resource useful for your research, please consider citing [this publication](https://doi.org/10.1099/mgen.0.001483) in your work.'        
         }
       ]
     }
