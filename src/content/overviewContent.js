c1a27f77e8e84e3288fdfda6266b566861f4327f
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2026-02-11 15:59:05 +0000

    Unify spelling

--- src/content/overviewContent.js
+++ src/content/overviewContent.js
@@ -5,7 +5,7 @@ const content = {
       content: [
         {
           type: 'md',
-          content: 'The GPS project provides a suite of open-source bioinformatics tools, curated databases, and step-by-step tutorials designed for pneumococcal genomic surveillance and research. We have established a dedicated GitHub page to showcase our latest developments, facilitating transparent and reproducible research worldwide.',
+          content: 'The GPS project provides a suite of open source bioinformatics tools, curated databases, and step-by-step tutorials designed for pneumococcal genomic surveillance and research. We have established a dedicated GitHub page to showcase our latest developments, facilitating transparent and reproducible research worldwide.',
         },
         {
           type: 'button',
