6639510dad2b58f0d0c09eeab3a8d982143e72d9
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2026-02-11 16:26:31 +0000

    Remove GPS Tools section

--- src/components/Training.jsx
+++ src/components/Training.jsx
@@ -1,11 +1,11 @@
-import GPSTools from "./GPSTools"
+import Tutorials from "./Tutorials"
 import BioinformaticsTraining from "./BioinformaticsTraining"
 import DragAndDropTools from "./DragAndDropTools"
 import CommandLine from "./CommandLine"
 
 const Training = () => (
   <div className="h-full">
-    <section id="gps-tools"><GPSTools /></section>
+    <section id="tutorials"><Tutorials /></section>
     <section id="command-line"><CommandLine /></section>
     <section id="drag-and-drop-tools"><DragAndDropTools /></section>
     <section id="bioinformatics-training"><BioinformaticsTraining /></section>
